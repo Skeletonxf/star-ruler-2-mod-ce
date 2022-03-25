Skeletonxf/star-ruler-2-mod-ce

commit e7cc73a7fc353f07ce88280ac3589b2ba03307a3

Fix shipset getting reset when reopening the customisation options in the lobby

colonisation-expansion/scripts/menu/new_game.as
@@ -1518,6 +1518,13 @@ class RaceChooser : GuiOverlay {
 		backButton.font = FT_Medium;
 		backButton.buttonIcon = icons::Back;
 
+		// [[ MODIFY BASE GAME START ]]
+		// When reopening the RaceChooser we may already have changed our
+		// shipset so don't allow `selectRace` to reset it.
+		if (!isInitial) {
+			chosenShipset = true;
+		}
+		// [[ MODIFY BASE GAME END ]]
 		selectRace(curSelection);
 		updateAbsolutePosition();
 		updateAbsolutePosition();

commit b415d6ba2b8d816ab96da0c23a98ce825fdeb8aa

Fix bug introduced in e7cc73a when trying to fix vanilla shipset selection bug

colonisation-expansion/scripts/menu/new_game.as
@@ -1542,7 +1542,7 @@ class RaceChooser : GuiOverlay {
 		GuiOverlay::close();
 	}
 
-	void selectRace(uint select) {
+	void selectRace(uint select, bool isManual = false) { // [[ MODIFY BASE GAME ]]
 		for(uint i = 0, cnt = presetButtons.length; i < cnt; ++i)
 			presetButtons[i].pressed = i == select;
 
@@ -1577,7 +1577,15 @@ class RaceChooser : GuiOverlay {
 		descScroll.updateAbsolutePosition();
 		@selectedRace = preset;
 
-		if(!chosenShipset) {
+		// [[ MODIFY BASE GAME START ]]
+		// We need to select the preset's shipset when the user changes their preset
+		// but not when the user just wants to customise their existing race. Above
+		// we set chosenShipset to true when the user reopens the RaceChooser
+		// which then also turns off selecting preset shipsets, so we need to
+		// select the preset shipset anyway if the user manually switches between
+		// presets.
+		if(!chosenShipset || isManual) {
+			// [[ MODIFY BASE GAME END ]]
 			setup.settings.shipset = preset.shipset;
 			for(uint i = 0, cnt = shipsets.length; i < cnt; ++i) {
 				if(shipsets.items[i].ident == preset.shipset) {
@@ -1666,7 +1674,9 @@ class RaceChooser : GuiOverlay {
 				for(uint i = 0, cnt = presetButtons.length; i < cnt; ++i) {
 					if(evt.caller.isChildOf(presetButtons[i])) {
 						hasChosenRace = true;
-						selectRace(i);
+						// [[ MODIFY BASE GAME START ]]
+						selectRace(i, isManual = true);
+						// [[ MODIFY BASE GAME END ]]
 						return true;
 					}
 				}